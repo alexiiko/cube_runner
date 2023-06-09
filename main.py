6bac59f7cccd55f775b59d17df639cd7de9d8a8e
alexiiko <alexiiko.msl@gmail.com>
2023-06-09 16:46:53 +0200

    Changed some images and implemented much code for the game. Such as player movement and game loop

--- main.py
+++ main.py
@@ -2,5 +2,95 @@ import pygame
 import os
 from sys import exit
 
-#TODO: make main loop
-#TODO: make player sprite 
+#TODO: implement horizontal and vertical collision for player with platform 
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Cube Runner")
+
+background_surface = pygame.image.load(os.path.join("OneDrive","Desktop","cube_runner","graphics", "background.png")).convert()
+background_surface_smaller = pygame.transform.scale(background_surface,(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ground_surface = pygame.image.load(os.path.join("OneDrive","Desktop","cube_runner","graphics", "ground.png")).convert()
+ground_surface_bigger = pygame.transform.scale(ground_surface, (SCREEN_WIDTH, 100))
+ground_rect = ground_surface_bigger.get_rect()
+
+platform_surface = pygame.image.load(os.path.join("OneDrive","Desktop","cube_runner","graphics", "platform.png")).convert_alpha()
+platform_rect = platform_surface.get_rect(center = (650,450))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        alive = pygame.image.load(os.path.join("OneDrive","Desktop","cube_runner","graphics", "player.png")).convert()
+        dead = pygame.image.load(os.path.join("OneDrive","Desktop","cube_runner","graphics", "player_dead.png")).convert()
+        self.state = [alive, dead]
+        self.index = 0 
+
+        self.image = self.state[self.index]
+        self.rect = self.image.get_rect(midbottom = (50, 550))
+        self.gravity = 0
+        self.fall_value = 0
+
+    def increase_fall_value(self):
+        if self.rect.bottom < 550:
+            self.fall_value += 1
+        if self.rect.bottom == 550: 
+           self.fall_value = 0
+
+    def border(self):
+        if self.rect.right >= 800:
+            self.rect.right = 800
+        if self.rect.left <= 0:
+            self.rect.left = 0
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.rect.x += 10
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.rect.x -= 10
+        if keys[pygame.K_SPACE] and self.fall_value <= 0:
+            self.gravity = -20
+        
+
+    def fall(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 550:
+            self.rect.bottom = 550
+
+    def horizontal_collision(self):
+        None
+    
+    def update(self):
+        self.horizontal_collision()
+        self.border()
+        self.increase_fall_value()
+        self.fall()
+        self.movement()
+
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+def draw_window():
+    screen.blit(background_surface_smaller,(0,0))
+    screen.blit(ground_surface_bigger,(0,550), ground_rect)
+    screen.blit(platform_surface, platform_rect)
+    player.draw(screen)
+    player.update()
+
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    draw_window()
+    pygame.display.update()
+    clock.tick(60)
